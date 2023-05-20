a5cc07868e1d00c8282cbf49191f56a81a52d0ba
zyzle <colin.mcculloch@komi.io>
2023-05-20 16:55:45 +0100

    chore(apps): moving apps/libs

--- apps/gateway/jest.config.ts
+++ apps/gateway/jest.config.ts
@@ -7,5 +7,5 @@ export default {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/gateway',
+  coverageDirectory: '../../coverage/apps/gateway',
 };
--- apps/orders/jest.config.ts
+++ apps/orders/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'products',
+  displayName: 'orders',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/products',
+  coverageDirectory: '../../coverage/apps/orders',
 };
